35f61a08039eba49618401c2a579556fca3c89fd
s3620166 <s3620166@vuw.leidenuniv.nl>
2025-04-06 23:33:29 +0200

    added main.cc and bfs.cu

---
+++ bfs.cu
@@ -0,0 +1,9 @@
+/* Tim Zuijderduijn (s3620166) 2025
+   bfs.cu
+*/
+
+extern "C" {
+
+  __global__ void runBFS();
+ 
+}
---
+++ main.cc
@@ -0,0 +1,13 @@
+/* Tim Zuijderduijn (s3620166) 2025
+   main.cc
+*/
+
+#include <cstdio>
+#include <cuda.h>
+
+int main() {
+
+  cuInit(0);
+  return 0;
+
+} // main
